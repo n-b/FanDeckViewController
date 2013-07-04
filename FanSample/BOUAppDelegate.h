084aef4991c6ea07bcc18f82c63cc5835bd6bdd2
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-07-04 09:00:28 +0200

    Initial Commit

---
+++ FanSample/BOUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BOUAppDelegate.h
+//  FanSample
+//
+//  Created by Nicolas on 04/07/13.
+//  Copyright (c) 2013 Nicolas Bouilleaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BOUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FanSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FanSample
+//
+//  Created by Nicolas on 04/07/13.
+//  Copyright (c) 2013 Nicolas Bouilleaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BOUAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BOUAppDelegate class]));
+    }
+}
